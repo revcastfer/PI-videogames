13498b3620f93ead366c5311eacde3f4f1a5aca8
fer <revcast@hotmail.com>
2023-06-08 01:45:37 -0500

    connect bd

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { videogame } = sequelize.models;
+const { genres } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+    name: {type: DataTypes.STRING,allowNull: false},
+    description:{type:DataTypes.STRING,allowNull:false},
+    plataforms:{type:DataTypes.STRING,allowNull:false},
+    image:{type:DataTypes.STRING,allowNull:false},
+    date:{type:DataTypes.DATEONLY,allowNull:false},
+    rating:{type:DataTypes.DOUBLE,allowNull:false},
+
   });
 };
